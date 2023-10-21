13bfd93ec9164bba7dab605ac27b7ded4f37b65f
G4ND4LFpl <71659952+G4ND4LFpl@users.noreply.github.com>
2023-10-22 00:31:17 +0200

    Initial commit

---
+++ DutchServisMCV/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DutchServisMCV.Models;
+
+namespace DutchServisMCV.Controllers
+{
+    public class HomeController : Controller
+    {
+        DutchDatabaseEntities database = new DutchDatabaseEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Users userInfo)
+        {
+            var logincheck = database.Users.Where(x => x.Username.Equals(userInfo.Username) && x.Pass.Equals(userInfo.Pass)).FirstOrDefault();
+            if(logincheck != null)
+            {
+                Session["Username"] = userInfo.Username.ToString();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Notification = "Nieprawidłowa nazwa lub hasło";
+            }
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Players()
+        {
+            return View(database.Players);
+        }
+    }
+}
---
+++ DutchServisMCV/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DutchServisMCV.MvcApplication" Language="C#" %>
