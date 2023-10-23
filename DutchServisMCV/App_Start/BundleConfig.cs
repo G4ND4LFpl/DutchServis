fc605933ce9cf7758e0e26052e3f5b7965c7ebbd
G4ND4LFpl <71659952+G4ND4LFpl@users.noreply.github.com>
2023-10-23 17:35:32 +0200

    nav done
    Full design of navigation bar

--- DutchServisMCV/App_Start/BundleConfig.cs
+++ DutchServisMCV/App_Start/BundleConfig.cs
@@ -23,8 +23,8 @@ namespace DutchServisMCV
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/css/bootstrap-light.css",
+                      "~/Content/css/site.css"));
         }
     }
 }
